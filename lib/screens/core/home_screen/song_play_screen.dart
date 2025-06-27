3147e03e1d62f3e2f4f080a805a857faf66b1b9b
InzaSanam08 <inzasanam7@gmail.com>
2025-06-27 09:54:08 -0700

    VPlayer

--- lib/screens/core/home_screen/song_play_screen.dart
+++ lib/screens/core/home_screen/song_play_screen.dart
@@ -17,121 +17,141 @@ class _Now_Playing_ScreenState extends State<Now_Playing_Screen> {
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
       builder: (context, value, child) {
-        final List<Song> playList = value.playlists;
 
-        return SafeArea(
-          child: Scaffold(
-            backgroundColor: Theme.of(context).colorScheme.surface,
-            body: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(Icons.arrow_back),
+        final  playList = value.playlists;
+        final currentSongIndex = playList[value.currentsongIndex ?? 0];
+
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                  Text(
+                    'P L A Y  L I S T',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.sp,
                     ),
-                    Text('P L A Y  L I S T'),
-                    IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-                  ],
-                ),
-                Neu_Box(
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.asset('assets/images/album_cover.png'),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Kabera',
-                                  style: TextStyle(
-                                    fontSize: 20.sp,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'Arijit Singh',
-                                  // style: TextStyle(
-                                  //   fontSize: 16.sp,
-                                  //   fontWeight: FontWeight.bold,
-                                  // ),
-                                ),
-                              ],
-                            ),
-                            Icon(Icons.favorite, color: Colors.red, size: 30),
-                          ],
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                ],
+              ),
+              SizedBox(height: 20.h),
+              // Displaying the current song image and details
+              Neu_Box(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.r),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.r),
+                        child: Image.asset(
+                          height: 230.h,
+                          currentSongIndex.albumArtImagePath,                    
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20.h),
+                    ),
 
-                Column(
-                  children: [
                     Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      padding: EdgeInsets.only(
+                        left: 15.w,
+                        right: 15.w,
+                        top: 15.h,
+                      ),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text('0:00'),
-                          Icon(Icons.shuffle),
-                          Icon(Icons.repeat),
-                          Text('0:00'),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                currentSongIndex.songName,
+                                style: TextStyle(
+                                  fontSize: 20.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                currentSongIndex.artistName,
+                            
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.favorite, color: Colors.red, size: 25.sp),
                         ],
                       ),
                     ),
-
-                    SliderTheme(
-                      data: SliderThemeData().copyWith(
-                        thumbShape: RoundSliderThumbShape(
-                          enabledThumbRadius: 0.r,
-                        ),
-                      ),
-                      child: Slider(
-                        min: 0,
-                        max: 100,
-                        value: 50,
-                        onChanged: (value) {},
-                        activeColor: Colors.green,
-                      ),
-                    ),
                   ],
                 ),
+              ),
+              SizedBox(height: 20.h),
 
-                SizedBox(height: 10.h),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('0:00'),
+                        Icon(Icons.shuffle),
+                        Icon(Icons.repeat),
+                        Text('0:00'),
+                      ],
+                    ),
+                  ),
 
-                Row(
+                  SliderTheme(
+                    data: SliderThemeData().copyWith(
+                      thumbShape: RoundSliderThumbShape(
+                        enabledThumbRadius: 0.r,
+                      ),
+                    ),
+                    child: Slider(
+                      min: 0,
+                      max: 100,
+                      value: 50,
+                      onChanged: (value) {},
+                      activeColor: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 10.h),
+
+              Container(
+                height: 50.h,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Expanded(
-                      flex: 2,
                       child: GestureDetector(
                         onTap: () {},
                         child: Neu_Box(child: Icon(Icons.skip_previous)),
                       ),
                     ),
 
-                    SizedBox(height: 20.h),
+                    SizedBox(width: 20.h),
 
                     Expanded(
+                      flex: 2,
                       child: GestureDetector(
                         onTap: () {},
                         child: Neu_Box(child: Icon(Icons.play_arrow)),
                       ),
                     ),
 
-                    SizedBox(height: 20.h),
+                    SizedBox(width: 20.h),
 
                     Expanded(
                       child: GestureDetector(
@@ -141,37 +161,37 @@ class _Now_Playing_ScreenState extends State<Now_Playing_Screen> {
                     ),
                   ],
                 ),
-              ],
-            ),
-
-            // Padding(
-            //   padding: const EdgeInsets.all(15.0),
-            //   child: Row(
-            //        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //     children: [
-            //       Column(
-            //     mainAxisAlignment: MainAxisAlignment.start,
-            //         children: [
-            //         Text(
-            //           'Kabera',
-            //         style: TextStyle(
-            //           fontSize: 20,
-            //           fontWeight: FontWeight.bold,
-            //         ),
-            //         ),
-            //          Text(
-            //           'Arijit Singh',
-            //         // style: TextStyle(
-            //         //   fontSize: 16,
-            //         //   fontWeight: FontWeight.bold,
-            //         // ),
-            //         ),
-            //         ],
-            //       ), Icon(Icons.favorite, color: Colors.red, size: 30,),
-            //     ],
-            //   ),
-            // )
+              ),
+            ],
           ),
+
+          // Padding(
+          //   padding: const EdgeInsets.all(15.0),
+          //   child: Row(
+          //        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       Column(
+          //     mainAxisAlignment: MainAxisAlignment.start,
+          //         children: [
+          //         Text(
+          //           'Kabera',
+          //         style: TextStyle(
+          //           fontSize: 20,
+          //           fontWeight: FontWeight.bold,
+          //         ),
+          //         ),
+          //          Text(
+          //           'Arijit Singh',
+          //         // style: TextStyle(
+          //         //   fontSize: 16,
+          //         //   fontWeight: FontWeight.bold,
+          //         // ),
+          //         ),
+          //         ],
+          //       ), Icon(Icons.favorite, color: Colors.red, size: 30,),
+          //     ],
+          //   ),
+          // )
         );
       },
     );
